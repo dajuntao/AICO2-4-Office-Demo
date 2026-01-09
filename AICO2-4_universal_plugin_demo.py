7107fbbfbae7f99b08ffd15d7e1c56178d69ad3e
Dajun Tao <dajun.tao@flexiv.com>
2026-01-08 16:30:52 -0800

    update comments

--- AICO2-4_universal_plugin_demo.py
+++ AICO2-4_universal_plugin_demo.py
@@ -43,14 +43,14 @@ def connect_arm_pair(arm_L_sn, arm_R_sn, logger):
         logger.info("[Arm] Fault on the connected arm is cleared")
 
     # Enable the pair of robots, make sure the E-stop is released before enabling
-    logger.info("Enabling arms ...")
+    logger.info("[Arm] Enabling arms ...")
     arm_pair.Enable()
 
     # Wait for the arm to become operational
     while not arm_pair.operational():
         time.sleep(1)
 
-    logger.info("Both arms are now operational")
+    logger.info("[Arm] Both arms are now operational")
 
     return arm_pair
 
@@ -59,31 +59,31 @@ def connect_arm_pair(arm_L_sn, arm_R_sn, logger):
 def connect_AMR(AMR_ip, logger):
     AMR_states = flexivamr.StatesAPI()
     if (not AMR_states.connect(AMR_ip)):
-        logger.warn("states failed to connect")
+        logger.warn("[Base] states failed to connect")
         return 1
     else:
-        logger.info("states connected successfully")
+        logger.info("[Base] states connected successfully")
 
     navigator = flexivamr.NavigatorAPI()
     if (not navigator.connect(AMR_ip)):
-        logger.warn("navigator failed to connect")
+        logger.warn("[Base] navigator failed to connect")
         return 1
     else:
-        logger.info("navigator connected successfully")
+        logger.info("[Base] navigator connected successfully")
 
     control = flexivamr.ControlAPI()
     if (not control.connect(AMR_ip)):
-        logger.warn("control failed to connect")
+        logger.warn("[Base] control failed to connect")
         return 1
     else:
-        logger.info("control connected successfully")
+        logger.info("[Base] control connected successfully")
 
     configure = flexivamr.ConfigureAPI()
     if (not configure.connect(AMR_ip)):
-        logger.warn("configure failed to connect")
+        logger.warn("[Base] configure failed to connect")
         return 1
     else:
-        logger.info("configure connected successfully")
+        logger.info("[Base] configure connected successfully")
 
     # seize AMR control and check relocation
     init_AMR(AMR_states, control, configure, logger)
@@ -93,22 +93,22 @@ def connect_AMR(AMR_ip, logger):
 
 # seize AMR control and check relocation 
 def init_AMR(states, control, configure, logger):
-    logger.info("Initializing the mobile robot...")
-    name = "AMR1"
+    logger.info("[Base] Initializing the mobile robot...")
+    name = "MyAICO2-4"
 
     # gain control over amr
     bool_control = configure.gain_control(name)
     if bool_control:
-        logger.info("control seized successfully")
+        logger.info("[Base] control seized successfully")
     else:
-        logger.warn("control failed to seize")
+        logger.warn("[Base] control failed to seize")
     
     # check if amr states::control is seized
     bool_control = states.is_control_seized()
     if bool_control:
-        logger.info("states::control is seized")
+        logger.info("[Base] states::control is seized")
     else:
-        logger.warn("states::control is not seized")
+        logger.warn("[Base] states::control is not seized")
     
     # clear faults
     #configure.clear_errors(name)
@@ -123,16 +123,16 @@ def init_AMR(states, control, configure, logger):
 
         control.perform_relocation(reloc)
         while states.check_relocation_status().reloc_status == 2:
-            logger.info("relocalizing...")
+            logger.info("[Base] relocalizing...")
             time.sleep(1)
 
         # AMR Version 3.4.6.18 and above do not require calling the confirmation positioning API again.
         # control.confirm_correct_location()  
 
     if states.check_relocation_status().reloc_status == 1:
-        logger.info("AMR is relocalized")
+        logger.info("[Base] AMR is relocalized")
     else:
-        raise Exception("AMR cannot be relocalized")
+        raise Exception("[Base] AMR cannot be relocalized")
 
 
 # execute routines with arm plans and move Seer AMR
@@ -143,40 +143,40 @@ def execute_routines(arm_pair, AMR_states, navigator, arm_plans, logger):
 
         # initialize gripper
         execute_arm_plan([arm_plans['gripper_init'] + 'L', arm_plans['gripper_init'] + 'R'], arm_pair, logger)
-        logger.info("Both Grippers are initialized.")
+        logger.info("[Arm] Both Grippers are initialized.")
 
         # move arm to the home pose and reset gripper width
         execute_arm_plan([arm_plans['arm_homing'] + 'L', arm_plans['arm_homing'] + 'R'], arm_pair, logger)
-        logger.info("Reset Arms home pose and Grippers home width.")
+        logger.info("[Arm] Reset Arms home pose and Grippers home width.")
         
         # move AMR to the universal plug-in station (LM3)
         move_AMR(AMR_states, navigator, logger, start="AP1", target="LM3")
-        logger.info("Move to plug-in station.")
+        logger.info("[Base] Moved to plug-in station.")
 
         # calibrated work coordinate and synch the new work coordinate in both arms
         execute_arm_plan([arm_plans['work_coord_calib'] + 'L', arm_plans['wait'] + 'R'], arm_pair, logger)
         R_arm.SetGlobalVariables({'workCoord': arm_pair.global_variables()[0]['workCoord']}) # synch the left arm work coordinate with the right arm
-        logger.info("Work coordinate calibrated and synched.")
+        logger.info("[Arm] Work coordinate calibrated and synched.")
 
         # move arm to transition + pickup BNC
         execute_arm_plan([arm_plans['transition_coord'] + 'L', arm_plans['pick-up'] + 'BNC_R'], arm_pair, logger)
-        logger.info("Moves Left arm to transition pose while the right arm picks up BNC.")
+        logger.info("[Arm] Moved Left arm to transition pose and the right arm pick up BNC.")
 
         # move arm to pickup 500v + curl
         execute_arm_plan([arm_plans['pick-up'] + '500v_L', arm_plans['curl-up'] + 'R'], arm_pair, logger)
-        logger.info("Moves Left arm to pickup the 500v connector pose while the right arm curls up to clear obstacles.")
+        logger.info("[Arm] Moved Left arm to pickup the 500v connector pose and the right arm curled-up to clear obstacles.")
 
         # move to plug-in + plug-in
         execute_arm_plan([arm_plans['plug-in'] + '500v_L', arm_plans['plug-in'] + 'BNC_R'], arm_pair, logger)
-        logger.info("Moves Left arm to plug in the 500v connector pose while the right arm plugs-in to BNC.")
+        logger.info("[Arm] Moved Left arm to plug in the 500v connector pose and the right arm pluged-in to BNC.")
 
         # move arm to the home pose and reset gripper width
         execute_arm_plan([arm_plans['arm_homing'] + 'L', arm_plans['arm_homing'] + 'R'], arm_pair, logger)
-        logger.info("Reset Arms home pose and Grippers home width.")
+        logger.info("[Arm] Reset Arms home pose and Grippers home width.")
 
         # move AMR back to home (AP1)
         move_AMR(AMR_states, navigator, logger, start="LM3", target="AP1")
-        logger.info("Move back to home.")
+        logger.info("[Base] Moved back to home.")
 
     except KeyboardInterrupt:
         raise KeyboardInterrupt("Routine is canceled")
@@ -200,19 +200,19 @@ def execute_arm_plan(plan_names, arm_pair, logger):
                 break
 
         # execute plans by name
-        logger.info(f"Left Arm: execute {plan_names[0]}")
-        logger.info(f"Right Arm: execute {plan_names[1]}")
+        logger.info(f"[Arm] Left Arm: execute {plan_names[0]}")
+        logger.info(f"[Arm] Right Arm: execute {plan_names[1]}")
         arm_pair.ExecutePlan(plan_names, False)
 
         # print current plans output
         while arm_pair.busy():
             left_arm_plan_info, right_arm_plan_info = arm_pair.plan_info()
             logger.info(" ")
-            print(f"left_plan_name: {left_arm_plan_info.assigned_plan_name}")
-            print(f"left_node_name: {left_arm_plan_info.node_name}")
+            print(f"[Arm] left_plan_name: {left_arm_plan_info.assigned_plan_name}")
+            print(f"[Arm] left_node_name: {left_arm_plan_info.node_name}")
             # print(f"left_pt_name: {left_arm_plan_info.pt_name}")
-            print(f"right_plan_name: {right_arm_plan_info.assigned_plan_name}")
-            print(f"right_node_name: {right_arm_plan_info.node_name}")
+            print(f"[Arm] right_plan_name: {right_arm_plan_info.assigned_plan_name}")
+            print(f"[Arm] right_node_name: {right_arm_plan_info.node_name}")
             # print(f"right_pt_name: {right_arm_plan_info.pt_name}")
             # print(f"node_path: {left_arm_plan_info.node_path}")
             # print(f"node_path_time_period: {left_arm_plan_info.node_path_time_period}")
@@ -232,12 +232,12 @@ def execute_arm_plan(plan_names, arm_pair, logger):
             time.sleep(1)
     except KeyboardInterrupt:
         # Send signal to exit thread
-        logger.info("Stopping plans execute thread")
+        logger.info("[Arm] Stopping plans execute thread")
         stop_event.set()
 
     # Wait for thread to exit
     plans_execute_thread.join()
-    logger.info("Plans execute thread exited")
+    logger.info("[Arm] Plans execute thread exited")
 
 
 # move Seer AMR
@@ -250,21 +250,17 @@ def move_AMR(states, navigator, logger, start, target):
         while (navi_states.task_status != 4 or navi_states.task_status != 6) and not exit_input.is_set(): # not finish and not canceled and event is not exited
             if select.select([sys.stdin], [], [], 1)[0]:    # check if sys.stdin is ready to be read with a timeout of 0 (non-blocking)
                 interruption = sys.stdin.readline().strip()
-                if interruption.lower() == 'c':
-                    navigator.cancel_current_navigation()
-                    exit_input.set()    # exit the event for thread interruption
-                elif interruption.lower() == 'p':
+                if interruption.lower() == 'p':
                     navigator.pause_current_navigation()
-                    logger.info("path navigation paused")
+                    logger.info("[Base] path navigation paused")
                 elif interruption.lower() == 'r':
                     navigator.continue_current_navigation()
-                    logger.info("path navigation resumed")
+                    logger.info("[Base] path navigation resumed")
             else:    
                 time.sleep(0.1)
 
-    logger.info("\'c + ENTER\' to cancel the current amr navigation")
-    logger.info("\'p + ENTER\' to pause  ..........................")
-    logger.info("\'r + ENTER\' to resume ..........................")
+    logger.info("\'p + ENTER\' to pause the current navigation")
+    logger.info("\'r + ENTER\' to resume .....................")
     
     # fixed path navigation
     path = navigator.PathNavigationCommand()
@@ -279,25 +275,21 @@ def move_AMR(states, navigator, logger, start, target):
     input_thread = threading.Thread(target=non_blocking_input_handler)
     input_thread.start()
 
-    logger.info("navi start status: " + str(navi_states.task_status))
+    logger.info("[Base] navi start status: " + str(navi_states.task_status))
     try:
         while ((navi_states.task_status == 2 or navi_states.task_status != 4) and navi_states.task_status != 6):
             time.sleep(1)
             navi_states = states.check_navigation_status()
-            logger.info("target wp: " + str(navi_states.target_id))
-            logger.info("navi status: " + str(navi_states.task_status))
-    except KeyboardInterrupt:
+            logger.info("[Base] target wp: " + str(navi_states.target_id))
+            logger.info("[Base] navi status: " + str(navi_states.task_status))
+    except KeyboardInterrupt: # use keyboardInterrupt to end the whole program
         navigator.cancel_current_navigation()
         exit_input.set()
         raise KeyboardInterrupt()
 
     # terminate thread and rejoin the thread
     exit_input.set()
-    input_thread.join()
-
-    # check navigation cancel status to terminate the execution
-    if navi_states.task_status == 6:
-        raise KeyboardInterrupt("path navigation canceled")    
+    input_thread.join()  
 
 
 # caller function
